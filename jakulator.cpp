77a14814cba43e9135df1201ba8c1252d2817a26
Fvcklms <fvcklms@gmail.com>
2020-10-14 18:03:21 +0800

    MOTHER FUCKER

---
+++ jakulator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+ int main () 
+ {
+ 	
+
+ 	int x,y,sum,diff,product,qoutient;
+ 	cout<<"1st number: ";
+ 	cin>>x;
+ 	cout<<"2nd number: ";
+ 	cin>>y;
+ 	cout<<endl;
+ 	sum = x+y;
+ 	diff = x-y;
+ 	product = x*y;
+ 	qoutient = x/y;
+ 	cout<<"SUM "<<sum<<endl;
+ 	cout<<"DIFF "<<diff<<endl;
+ 	cout<<"PRO "<<product<<endl;
+ 	cout<<"QUO "<<qoutient<<endl;
+ 	
+ 
+ }
